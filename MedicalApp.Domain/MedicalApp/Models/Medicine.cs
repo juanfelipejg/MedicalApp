e04a25a8be5988ba709b5b44c8212bdac37bcb5c
felipe jarmillo <felipe.jaramillo@booktofly.com.co>
2022-11-20 21:14:05 -0500

    Medicine and Record were created

---
+++ MedicalApp.Domain/MedicalApp/Models/Medicine.cs
@@ -0,0 +1,15 @@
+﻿namespace MedicalApp.Domain.MedicalApp.Models
+{
+    public class Medicine
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<string> Uses { get; set; }
+
+        public IEnumerable<string> AvailablePlaces { get; set; }
+
+        public decimal Price { get; set; }
+
+        public IEnumerable<string> Presentations { get; set; }
+    }
+}
